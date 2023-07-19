dfcab89777b3cb2f8de3f78013f4e28069f80e48
Moontlan <moontlan.dev@gmail.com>
2023-07-19 16:04:43 -0400

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,14 @@ module.exports = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'back': "url('https://www.orlandomm.me/noise.gif')"
       },
+      colors: {
+        p1: '#201e33',
+        p2: '#4da4a6',
+        p3: '#dc5b51'
+      }
     },
   },
   plugins: [],
